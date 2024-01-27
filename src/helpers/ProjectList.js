9431716a9a6b2e80f0b1ca8ed859e2db111ac30a
Jai_Vang <jaivang0387@gmail.com>
2024-01-27 18:04:18 -0500

    adding description to projects, chaning 'experience' to 'experiences'

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -13,7 +13,8 @@ export const ProjectList = [
     appURL: "https://webpages.charlotte.edu/jvang29/Musicians-Meetup/index.html",
     appAnchorTagTitle: "Musicians Meetup",
     gitHubUrl: "https://github.com/jvang0620/Musicians-Meetup",
-    anchorTagTitle: "Musicians Meetup GitHub"
+    anchorTagTitle: "Musicians Meetup GitHub",
+    projectDescription: "This Fullstack MVC Express.js application serves as a dynamic and engaging social media platform tailored for seamlessly hosting and coordinating both in-person and virtual activities, gatherings, and events. It provides a centralized hub for individuals and communities with shared interests, hobbies, and professions to come together and connect. Whether it's organizing a local meetup, planning a virtual workshop, or coordinating a professional networking event, the platform empowers users to create, discover, and participate in a wide array of activities that resonate with their passions and preferences. With its intuitive interface and robust features, the application fosters a sense of community, making it easier for like-minded individuals to build connections and share meaningful experiences."
   },
   {
     name: "Barhive",
@@ -22,16 +23,18 @@ export const ProjectList = [
     appURL: "https://barhive.net",
     appAnchorTagTitle: "Barhive",
     gitHubUrl: "https://github.com/jvang0620/Barhive",
-    anchorTagTitle: "Barhive GitHub"
+    anchorTagTitle: "Barhive GitHub",
+    projectDescription: "BarHive is a social networking platform for bars and people who love nightlife. It allows users to create profiles, connect with other users, and share posts related to their favorite bars and events. The application provides features such as creating posts, commenting, and liking posts. Users can also follow other users and view their profiles. The application also supports business profiles, which allows bar owners to create profiles for their bars and showcase their business."
   },
   {
     name: "Let's Chat",
     image: Proj3,
-    skills: "React, Express, Socket.io",
+    skills: "ReactJS, Express.JS, Socket.io, JavaScript, HTML, CSS",
     appURL: "https://main.d3rd3erfcsol2x.amplifyapp.com/",
     appAnchorTagTitle: "Let's Chat",
     gitHubUrl: "https://github.com/jvang0620/Lets-Chat",
-    anchorTagTitle: "Let's Chat GitHub"
+    anchorTagTitle: "Let's Chat GitHub",
+    projectDescription: "The Chat App is designed to provide users with an engaging and dynamic platform for real-time conversations. Leveraging state-of-the-art technologies, the application seamlessly unites users in interactive chat rooms, fostering a strong sense of community and facilitating smooth communication. With an emphasis on user experience, the app boasts a user-friendly interface that ensures a seamless and enjoyable chatting experience for individuals of all skill levels."
   },
   {
     name: "Calendar",
@@ -40,7 +43,8 @@ export const ProjectList = [
     appURL: "https://dev.dqbf8ff1peu9r.amplifyapp.com/",
     appAnchorTagTitle: "Calendar",
     gitHubUrl: "https://github.com/jvang0620/Calendar",
-    anchorTagTitle: "Calendar GitHub"
+    anchorTagTitle: "Calendar GitHub",
+    projectDescription: "Explore the Interactive JavaScript Calendar, a dynamic and engaging project. This calendar not only dynamically adjusts to the current month and year but also highlights holidays and observances, making it both functional and visually appealing. Discover the convenience of tooltips that provide additional information when you hover over highlighted dates. The program will be an ongoing project, and I'll continue to incorporate new elements over time."
   },
   {
     name: "P&J Wedding Rentals",
@@ -49,7 +53,8 @@ export const ProjectList = [
     appURL: "https://dev.ddp0hgzolmkk1.amplifyapp.com/",
     appAnchorTagTitle: "P&J Wedding Rentals",
     gitHubUrl: "https://github.com/jvang0620/P_J-Wedding-Rentals",
-    anchorTagTitle: "P&J Wedding Rentals GitHub"
+    anchorTagTitle: "P&J Wedding Rentals GitHub",
+    projectDescription: "Introducing our Wedding Events Showcase App, a beautifully crafted  application designed to elegantly display a wedding coordinators' events and greenery arrangements. This application serves as a visual portfolio, providing users with a captivating glimpse into the meticulous planning and exquisite floral designs curated by our talented wedding coordinators. The app delivers a user-friendly experience, allowing visitors to explore a gallery of stunning weddings and enchanting greenery setups. Immerse yourself in the world of unforgettable celebrations and inspired floral creations, brought to life through this thoughtfully designed and visually appealing Wedding Events Showcase App. Explore the artistry, creativity, and attention to detail that our wedding coordinators bring to every event, making each celebration a unique and memorable experience."
   },
   {
     name: "Tic-Tac-Toe",
@@ -58,6 +63,7 @@ export const ProjectList = [
     appURL: "https://dev.d1kjiaydz1x7jk.amplifyapp.com/",
     appAnchorTagTitle: "Tic-Tac-Toe",
     gitHubUrl: "https://github.com/jvang0620/Tic-Tac-Toe",
-    anchorTagTitle: "Tic-Tac-Toe GitHub"
+    anchorTagTitle: "Tic-Tac-Toe GitHub",
+    projectDescription: ""
   },
 ];
