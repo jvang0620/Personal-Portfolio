e5d4db288cba278cb05a53c224131d254645e86d
Jai_Vang <jaivang0387@gmail.com>
2024-01-26 21:37:24 -0500

    adding urls of app and github, fixing styles of project page

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -10,30 +10,54 @@ export const ProjectList = [
     name: "Musicians Meetup",
     image: Proj1,
     skills: "Node.js, Express.js, EJS, JavaScript, Bootstrap, MVC, MongoDB Atlas, Mongoose",
+    appURL: "https://webpages.charlotte.edu/jvang29/Musicians-Meetup/index.html",
+    appAnchorTagTitle: "Musicians Meetup",
+    gitHubUrl: "https://github.com/jvang0620/Musicians-Meetup",
+    anchorTagTitle: "Musicians Meetup GitHub"
   },
   {
     name: "Barhive",
     image: Proj2,
     skills: "AWS S3, Python, Flask, Jinja, PostgreSQL, SQLalchemy, HTML, CSS, JavaScript, Bootstrap",
+    appURL: "https://barhive.net",
+    appAnchorTagTitle: "Barhive",
+    gitHubUrl: "https://github.com/jvang0620/Barhive",
+    anchorTagTitle: "Barhive GitHub"
   },
   {
     name: "Let's Chat",
     image: Proj3,
     skills: "React, Express, Socket.io",
+    appURL: "https://main.d3rd3erfcsol2x.amplifyapp.com/",
+    appAnchorTagTitle: "Let's Chat",
+    gitHubUrl: "https://github.com/jvang0620/Lets-Chat",
+    anchorTagTitle: "Let's Chat GitHub"
   },
   {
-    name: "Calendar App",
+    name: "Calendar",
     image: Proj4,
     skills: "JavaScript, CSS, HTML",
+    appURL: "https://dev.dqbf8ff1peu9r.amplifyapp.com/",
+    appAnchorTagTitle: "Calendar",
+    gitHubUrl: "https://github.com/jvang0620/Calendar",
+    anchorTagTitle: "Calendar GitHub"
   },
   {
     name: "P&J Wedding Rentals",
     image: Proj5,
     skills: "JavaScript, HTML, CSS, JQuery plugins, JSON",
+    appURL: "https://dev.ddp0hgzolmkk1.amplifyapp.com/",
+    appAnchorTagTitle: "P&J Wedding Rentals",
+    gitHubUrl: "https://github.com/jvang0620/P_J-Wedding-Rentals",
+    anchorTagTitle: "P&J Wedding Rentals GitHub"
   },
   {
     name: "Tic-Tac-Toe",
     image: Proj6,
     skills: "JavaScript, HTML, CSS",
+    appURL: "https://dev.d1kjiaydz1x7jk.amplifyapp.com/",
+    appAnchorTagTitle: "Tic-Tac-Toe",
+    gitHubUrl: "https://github.com/jvang0620/Tic-Tac-Toe",
+    anchorTagTitle: "Tic-Tac-Toe GitHub"
   },
 ];
