929d8028f93e3c11d93dd5b88e181e9f3c156271
Jai_Vang <jaivang0387@gmail.com>
2024-03-13 13:14:39 -0400

    updating miniDescriptions

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -24,7 +24,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Musicians Meetup",
     gitHubUrl: "https://github.com/jvang0620/Musicians-Meetup",
     anchorTagTitle: "Musicians Meetup GitHub",
-    miniDescription: "Fullstack MVC Express.js App",
+    miniDescription: "Full-Stack Web Application",
     overview: "This Fullstack MVC Express.js application serves as a dynamic and engaging social media platform tailored for seamlessly hosting and coordinating both in-person and virtual activities, gatherings, and events. It provides a centralized hub for individuals and communities with shared interests, hobbies, and professions to come together and connect. Whether it's organizing a local meetup, planning a virtual workshop, or coordinating a professional networking event, the platform empowers users to create, discover, and participate in a wide array of activities that resonate with their passions and preferences. With its intuitive interface and robust features, the application fosters a sense of community, making it easier for like-minded individuals to build connections and share meaningful experiences."
   },
   {
@@ -35,7 +35,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Niner-Rate",
     gitHubUrl: "https://github.com/jvang0620/Niner-Rate-V2",
     anchorTagTitle: "Niner-Rate GitHub",
-    miniDescription: "Fullstack Next.js React App",
+    miniDescription: "Full-Stack Web Application",
     overview: "This Fullstack Next.js React Application serves as a sophisticated course and professor rating system designed specifically for University of North Carolina at Charlotte students. With a primary goal of empowering students with insightful information, the platform allows users to rate courses and instructors based on various criteria. Students can provide detailed feedback on the tech stack employed in courses, course materials, teaching methodologies, and class structures. By facilitating transparency in teaching styles, the application equips students with essential knowledge to navigate their academic journey effectively and prepare for their professional careers. It aims to mitigate the common issue of students enrolling in courses without a clear understanding of what to expect, subsequently avoiding potential regrets. Through the app, students can create, modify, and delete their reviews as needed, ensuring that their feedback remains relevant and up-to-date. Additionally, the platform offers a comprehensive overview of ratings for courses offered by the College of Computing and Informatics (CCI), providing students with valuable insights into the educational landscape within their academic domain. Ultimately, this application serves as a valuable resource for students seeking to make informed decisions and optimize their learning experiences."
   },
   {
@@ -46,7 +46,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Barhive",
     gitHubUrl: "https://github.com/jvang0620/Barhive",
     anchorTagTitle: "Barhive GitHub",
-    miniDescription: "Fullstack Python Flask App",
+    miniDescription: "Full-Stack Web Application",
     overview: "BarHive is a social networking platform for bars and people who love nightlife. It allows users to create profiles, connect with other users, and share posts related to their favorite bars and events. The application provides features such as creating posts, commenting, and liking posts. Users can also follow other users and view their profiles. The application also supports business profiles, which allows bar owners to create profiles for their bars and showcase their business."
   },
   {
@@ -57,7 +57,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Vite React TypeScript App",
     gitHubUrl: "https://github.com/jvang0620/Movie-Rating",
     anchorTagTitle: "Moving Rating GitHub",
-    miniDescription: "Vite React TypeScript App",
+    miniDescription: "Web Application",
     overview: "The Movie Rating App is a comprehensive web application that empowers users to discover and rate their favorite movies and TV shows. Built with a seamless integration with The Movie Database (TMDB) API, the app provides users with up-to-date information on an extensive library of titles, including current releases, popular picks, and timeless classics."
   },
   {
@@ -68,7 +68,7 @@ export const ProjectList = [
     appAnchorTagTitle: "React Portfolio",
     gitHubUrl: "https://github.com/jvang0620/Personal-Portfolio",
     anchorTagTitle: "Portfolio GitHub",
-    miniDescription: "React App",
+    miniDescription: "Web Application",
     overview: "This application is a culmination of creativity, dedication, and my journey of coding. As a computer science enthusiast and software engineering student, I've poured my passion for learning and creating into every line of code. Explore a collection of diverse projects ranging from full-stack applications to captivating web designs and interactive experiences. Each project reflects not only technical skills but also a commitment to crafting solutions that make a difference. Dive into the timeline of my experiences, from academic achievements to real-world challenges as a software developer intern. This journey encapsulates my growth, from assembling products with precision to crafting intricate software solutions."
   },
   {
@@ -79,7 +79,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Let's Chat",
     gitHubUrl: "https://github.com/jvang0620/Lets-Chat",
     anchorTagTitle: "Let's Chat GitHub",
-    miniDescription: "React Socket.io App",
+    miniDescription: "Web Application",
     overview: "The Chat App is designed to provide users with an engaging and dynamic platform for real-time conversations. Leveraging state-of-the-art technologies, the application seamlessly unites users in interactive chat rooms, fostering a strong sense of community and facilitating smooth communication. With an emphasis on user experience, the app boasts a user-friendly interface that ensures a seamless and enjoyable chatting experience for individuals of all skill levels."
   },
   {
@@ -90,7 +90,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Eight Queens",
     gitHubUrl: "https://github.com/jvang0620/Eight-Queens-CSharp",
     anchorTagTitle: "Eight Queens GitHub",
-    miniDescription: "C#, .Net, Java, Python Apps",
+    miniDescription: "Command-Line Interface Application",
     overview: "This terminal program implements a solution to the classic Eight-Queens problem using a hill climbing algorithm. The Eight-Queens problem involves placing 8 chess queens on an 8×8 chessboard so that no two queens threaten each other; thus, a solution requires that no two queens share the same row, column, or diagonal. This program was created from the ground up, drawing inspiration from various sources. I have developed three distinct projects utilizing C#/.NET, Java, and Python to showcase my proficiency in these languages."
   },
   {
@@ -101,7 +101,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Wedding Inventory App",
     gitHubUrl: "https://github.com/jvang0620/Wedding-Decor-Inventory-GUI",
     anchorTagTitle: "Wedding Inventory App GitHub",
-    miniDescription: "Java Swing App",
+    miniDescription: "Desktop GUI App",
     overview: "Welcome to our Wedding Decor Inventory Management System! This application is designed to streamline the process of managing inventory for wedding planners and decorators. With features tailored to the unique needs of wedding professionals, our system offers a comprehensive solution for organizing, tracking, and reporting on wedding decor items. With a focus on simplicity and functionality, our system facilitates the smooth handling of inventory tasks, allowing users to effortlessly input new items, track existing ones, and make necessary modifications as needed. From elegant vases to exquisite table runners and lush greenery, our system accommodates a variety of decor types to suit every wedding theme and style. Our Wedding Decor Inventory Management System offers a seamless experience with its intuitive interface, enabling users of all technical backgrounds to effortlessly navigate through features like viewing, creating, and updating inventory items. Easily add, modify, or delete items to maintain an accurate inventory, with the option to permanently delete or restore items as needed. Confirmation prompts add an extra layer of security, while detailed CSV reports provide insights into inventory data, supported by persistent storage for seamless continuity across sessions."
   },
   {
@@ -112,7 +112,7 @@ export const ProjectList = [
     appAnchorTagTitle: "A-Star Pathfinding",
     gitHubUrl: "https://github.com/jvang0620/A-Star-Pathfinding-Visualizer",
     anchorTagTitle: "A-Star Pathfinding GitHub",
-    miniDescription: "Java App",
+    miniDescription: "Command-Line Interface Application",
     overview: "This Java application provides a pathfinding tool based on the A\* algorithm, allowing users to find the shortest path between two points on a grid. The app prompts users to input coordinates for the starting and goal nodes within a 15x15 grid, ensuring the chosen coordinates are within bounds and not obstructed by obstacles. Upon receiving user input, the app employs the A\* algorithm to search for the optimal path, considering obstacles and the grid's dimensions. If a viable path exists, the app displays the coordinates of each node along the path and visualizes the agent's movement from the starting node to the goal node on the grid. This app serves as a practical tool for understanding and implementing pathfinding algorithms, offering a hands-on approach to visualize and comprehend their functionality. Whether for educational purposes or practical applications, users can utilize this tool to navigate and analyze pathfinding scenarios within a customizable grid environment."
   },
   {
@@ -123,7 +123,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Digital Melodies",
     gitHubUrl: "https://github.com/jvang0620/Digital-Melodies",
     anchorTagTitle: "Digital Melodies GitHub",
-    miniDescription: "Python App",
+    miniDescription: "Command-Line Interface Application",
     overview: "Drive into the fascinating realm of converting binary patterns into musical compositions. By extracting bytes from a binary file, we assign each byte to a specific musical note within the range of C0 to B8. Carefully sequenced, these mapped notes form an original piece of music inspired by the structural intricacies of the binary data. Our script orchestrates these notes into captivating melodies, breathing life into the data through the language of music."
   },
   {
@@ -134,7 +134,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Breadth-First Search",
     gitHubUrl: "https://github.com/jvang0620/Breadth-First-Search",
     anchorTagTitle: "Breadth-First Search GitHub",
-    miniDescription: "Python App",
+    miniDescription: "Command-Line Interface Application",
     overview: "This Python program implements a binary search tree with a breadth-first search algorithm to find the shortest path between two nodes. Users can specify the number of nodes in the tree, the number to search for, and the starting and ending points of the search. The program generates a random tree based on the user's input, and each time the program restarts, the location of the specified node is randomly determined. It then finds the shortest path from the starting point to the ending point while passing through the specified number."
   },
   {
@@ -145,7 +145,7 @@ export const ProjectList = [
     appAnchorTagTitle: "P&J Wedding Rentals",
     gitHubUrl: "https://github.com/jvang0620/P_J-Wedding-Rentals",
     anchorTagTitle: "P&J Wedding Rentals GitHub",
-    miniDescription: "JavaScript App",
+    miniDescription: "Web Application",
     overview: "Introducing our Wedding Events Showcase App, a beautifully crafted  application designed to elegantly display a wedding coordinators' events and greenery arrangements. This application serves as a visual portfolio, providing users with a captivating glimpse into the meticulous planning and exquisite floral designs curated by our talented wedding coordinators. The app delivers a user-friendly experience, allowing visitors to explore a gallery of stunning weddings and enchanting greenery setups. Immerse yourself in the world of unforgettable celebrations and inspired floral creations, brought to life through this thoughtfully designed and visually appealing Wedding Events Showcase App. Explore the artistry, creativity, and attention to detail that our wedding coordinators bring to every event, making each celebration a unique and memorable experience."
   },
   {
@@ -156,7 +156,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Pokemon Paper Rock Scissors",
     gitHubUrl: "https://github.com/jvang0620/Pokemon-Paper-Rock-Scissors",
     anchorTagTitle: "Pokemon Paper Rock Scissors GitHub",
-    miniDescription: "JavaScript App",
+    miniDescription: "Web Application",
     overview: "In 'Pokemon Rock Paper Scissors,' players engage in a strategic battle reminiscent of the classic game with a thrilling twist inspired by the iconic Pokemon universe. Choose your Pokemon avatar from the selections. Bulbasaur, Geodude, and Scyther will represent a different element - Paper, Rock, or Scissors. Pit your chosen Pokemon against your opponent's (Team Rocket) in an intense showdown where strategy and quick thinking are key. Will you emerge victorious, harnessing the strengths of your chosen Pokemon, or will you fall to the cunning tactics of your rival? Test your skills against the computer in this exciting adaptation of a beloved childhood game. First to win 5 matches is the winner. Get ready to embark on a journey filled with excitement, surprises, and the thrill of Pokemon battles in 'Pokemon Rock Paper Scissors'! May the best Pokemon trainer prevail!"
   },
   {
@@ -167,7 +167,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Tic-Tac-Toe",
     gitHubUrl: "https://github.com/jvang0620/Tic-Tac-Toe",
     anchorTagTitle: "Tic-Tac-Toe GitHub",
-    miniDescription: "Interactive UI/UX App",
+    miniDescription: "Web Application",
     overview: "Korok Tic-Tac-Toe is a playful rendition of the classic game, infused with the charm of the Korok characters from the beloved Legend of Zelda series. The game provides an engaging and interactive experience, inviting players to enjoy a timeless pastime with a delightful twist. It is not just a game; it's an enchanting journey into the world of the Koroks, where every move brings joy and excitement. Join the adventure and see if you have what it takes to emerge victorious in this whimsical twist on a timeless classic."
   },
   {
@@ -178,7 +178,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Calendar",
     gitHubUrl: "https://github.com/jvang0620/Calendar",
     anchorTagTitle: "Calendar GitHub",
-    miniDescription: "JavaScript App",
+    miniDescription: "Web Application",
     overview: "This clock app provides a simple yet elegant way to visualize the current time. With its sleek design and smooth animations, it brings the timeless charm of traditional clocks to the digital world"
   },
   {
@@ -189,8 +189,7 @@ export const ProjectList = [
     appAnchorTagTitle: "Clock",
     gitHubUrl: "https://github.com/jvang0620/Clock",
     anchorTagTitle: "Clock GitHub",
-    miniDescription: "JavaScript App",
+    miniDescription: "Web Application",
     overview: "Explore the Interactive JavaScript Calendar, a dynamic and engaging project. This calendar not only dynamically adjusts to the current month and year but also highlights holidays and observances, making it both functional and visually appealing. Discover the convenience of tooltips that provide additional information when you hover over highlighted dates. The program will be an ongoing project, and I'll continue to incorporate new elements over time."
   },
-
 ];
